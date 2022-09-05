aa797cdfa26d1738dcd1ac6c31f9705a1b8892c4
ibbob90@gmail.com <ibbob90@gmail.com>
2022-09-05 14:36:42 +0100

    Add existing file

---
+++ resume/resume.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+func mydetails(name  string, age int, phonenumber string, height float64) string {
+	//	ibinabo, 24, +1238148969926, 5'9
+
+	result := fmt.Sprintf("my name is %s i am %d years old my phonenumber %s my height is %f", name, age, phonenumber, height)
+	return result
+}
+func main(){
+	var result = mydetails("ibinabo", 24, "+1238148969926", 5.8)
+	fmt.Println(result)
+}
