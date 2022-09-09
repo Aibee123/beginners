ff54794a842127d4a4e62d23392030f33a9664da
ibbob90@gmail.com <ibbob90@gmail.com>
2022-09-09 12:31:42 +0100

    if statement

--- learngo/main.go
+++ learngo/main.go
@@ -2,16 +2,10 @@ package main
 
 import(
 		"fmt"
-		"reflect"
 )
 
-type Animal struct {
-		Name string 	`required max:"100"`
-		Origin string
-}
-
 func main() {
-		t := reflect.TypeOf(Animal{})
-		field, _ := t.FieldByName("Name")
-		fmt.Println(field.Tag)
+		if true {
+				fmt.Println("The test is true")
+		}
 }
