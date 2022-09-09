4824b8e1c8fea2c8ea6dc1d1da6681762cb77c64
ibbob90@gmail.com <ibbob90@gmail.com>
2022-09-09 03:37:34 +0100

    internal struct

--- learngo/main.go
+++ learngo/main.go
@@ -4,23 +4,22 @@ import(
 		"fmt"
 )
 
-type Doctor struct {
-		number int
-		actorName string
-		companions []string
-		episodes []string
+type Animal struct {
+		Name string
+		Origin string
+}
+
+type Bird struct {
+		Animal
+		SpeedKPH  		float32
+		CanFly			bool
 }
 
 func main() {
-		aDoctor:= Doctor{
-				number: 3,
-				actorName: "Jon Pertwee",
-				episodes: []string {},
-				companions: []string {
-						"Liz Shaw",
-						"Jo Grant",
-						"Sarah Jane Smith",
-				},
-			}
-			fmt.Println(aDoctor.companions)
+		b := Bird{}
+		b.Name = "Emu"
+		b.Origin = "Australia"
+		b.SpeedKPH = 48
+		b.CanFly = false
+		fmt.Println(b.Name)
 }
