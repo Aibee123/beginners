8dda3a4bb8ede8d3ecfd93455d7a53eda758ee98
ibbob90@gmail.com <ibbob90@gmail.com>
2022-09-09 12:44:35 +0100

    too low

--- learngo/main.go
+++ learngo/main.go
@@ -5,7 +5,15 @@ import(
 )
 
 func main() {
-		if true {
-				fmt.Println("The test is true")
+		number := 50
+		guess := 30
+		if guess < number {
+				fmt.Println("Too low")
+		}
+		if guess > number {
+				fmt.Println("Too high")
+		}
+		if guess == number {
+				fmt.Println("You got it")
 		}
 }
