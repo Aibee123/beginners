aec21d4ffe1e7229e7d1a8641206801188c479c8
ibbob90@gmail.com <ibbob90@gmail.com>
2022-09-09 14:10:59 +0100

    Short circuting

--- learngo/main.go
+++ learngo/main.go
@@ -7,7 +7,7 @@ import(
 func main() {
 		number := 50
 		guess := -5
-		if guess < 1 || guess > 100 {
+		if guess < 1 || returnTrue() || guess > 100 {
 				fmt.Println("The guess must be between 1 and 100!")
 		}
 		if guess >= 1 && guess <= 100 {
@@ -15,11 +15,16 @@ func main() {
 						fmt.Println("Too low")
 				}
 				if guess > number {
-						fmt.Println("Too high")
+						fmt.Println("Too high" )
 				}
 				if guess == number {
 						fmt.Println("You got it")
 				}
 				fmt.Println(number<=guess, number >=guess, number!=guess)
 		}		
+}
+
+func returnTrue() bool {
+		fmt.Println("returning true")
+		return true
 }
