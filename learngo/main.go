a2a78cb3d592c809ddff6bdf0c3ef227f80ecb08
ibbob90@gmail.com <ibbob90@gmail.com>
2022-09-10 17:09:07 +0100

    add declared variable to if else if

--- learngo/main.go
+++ learngo/main.go
@@ -1,15 +1,16 @@
 package main
 
 import (
-	"fmt"
-	"math"
+		"fmt"
 )
 
 func main() {
-		myNum := 0.123
-		if math.Abs(myNum / math.Pow(math.Sqrt(myNum), 2) - 1) < 0.001 {
-				fmt.Println("These are the same")
-		} else {
-				fmt.Println("These are different")
+		switch i:= 2 + 3;i {
+		case 1, 5, 10:
+				fmt.Println("one, five or ten")
+		case 2, 4, 6:
+				fmt.Println("two, four or  six")
+		default:
+				fmt.Println("another number")
 		}
 }
