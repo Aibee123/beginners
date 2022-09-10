22b61e3d8f2b35878de5996837af6b0648bd44b3
ibbob90@gmail.com <ibbob90@gmail.com>
2022-09-10 17:24:23 +0100

    switch case

--- learngo/main.go
+++ learngo/main.go
@@ -5,12 +5,13 @@ import (
 )
 
 func main() {
-		switch i:= 2 + 3;i {
-		case 1, 5, 10:
-				fmt.Println("one, five or ten")
-		case 2, 4, 6:
-				fmt.Println("two, four or  six")
+		i := 10
+		switch {
+		case i <= 10 :
+				fmt.Println("less than or equal to ten")
+		case i <= 20:
+				fmt.Println("less than or equal to twenty")
 		default:
-				fmt.Println("another number")
+				fmt.Println("greater than twenty")
 		}
 }
