c25d2c6898771501d052535ad9efb95e899a86a8
ibbob90@gmail.com <ibbob90@gmail.com>
2022-09-10 18:27:04 +0100

    for loop

--- learngo/main.go
+++ learngo/main.go
@@ -5,17 +5,7 @@ import (
 )
 
 func main() {
-		var i interface {} = [3]int{}
-		switch i. (type) {
-		case int:
-				fmt.Println("i is an int")
-		case float64:
-				fmt.Println("i is a float64")
-		case string:
-				fmt.Println("i is a string")
-		case [3]int:
-				fmt.Println("i is [3]int")
-			default:
-				fmt.Println("i is another type")
+		for i := 0; i < 5; i++ {
+				fmt.Println(i)
 		}
-}
+}   
