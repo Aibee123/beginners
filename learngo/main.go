e65c25850eac57841ecf3bac2443670068b93133
ibbob90@gmail.com <ibbob90@gmail.com>
2022-09-12 21:36:36 +0100

    data text types

--- learngo/main.go
+++ learngo/main.go
@@ -5,8 +5,11 @@ import (
 )
 
 func main() {
-		sayMessage("Hello Go!")
+		for i := 0; i < 5; i++ {
+				sayMessage("Hello Go", i)
+		}
 }
-func sayMessage(msg string) {
+func sayMessage(msg string, idx int) {
 		fmt.Println(msg)
+		fmt.Println("The value of the index is", idx)
 }
