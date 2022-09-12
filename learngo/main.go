1edfb679513f818496d81564af7f08e868f827fc
ibbob90@gmail.com <ibbob90@gmail.com>
2022-09-12 13:57:44 +0100

    defer statement

--- learngo/main.go
+++ learngo/main.go
@@ -5,16 +5,7 @@ import (
 )
 
 func main() {
-		statePopulations := map[string]int{
-				"California":	39250017,
-				"Texas":  		27862596,
-				"Florida":		20612439,
-				"New York": 	19745289,
-				"Pennsylvania":	12802503,
-				"Illinois":		12801539,
-				"Ohio":		 	11614373,
-		}
-		for _, v := range statePopulations {
-				fmt.Println(v)
-		}
+		defer fmt.Println("start")
+		defer fmt.Println("middle")
+		defer fmt.Println("end")
 }
