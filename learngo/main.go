eb319a8ddf98e62a817b3dc5ef7f61ef4d946068
ibbob90@gmail.com <ibbob90@gmail.com>
2022-09-13 15:11:16 +0100

    variable incrementer

--- learngo/main.go
+++ learngo/main.go
@@ -5,17 +5,21 @@ import (
 )
 
 func main() {
-		var w Writer = ConsoleWriter{}
-		w.Write([]byte("Hello Go!"))
+		myInt := IntCounter(0)
+		var inc Incrementer = &myInt
+		for i := 0; i < 10; i++ {
+				fmt.Println(inc.Increment())
+		}
+
 }
 
-type Writer interface {
-		Write([]byte) (int, error)
+type Incrementer interface {
+		Increment() int
 }
 
-type ConsoleWriter struct {}
+type IntCounter int
 
-func (cw ConsoleWriter) Write(data []byte) (int, error) {
-		n, err := fmt.Println(string(data))
-		return n, err
+func (ic *IntCounter) Increment() int {
+		*ic++
+		return int(*ic)
 }
