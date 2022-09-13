b3c0aa8f5b71096061f041bbaaa47370578b1764
ibbob90@gmail.com <ibbob90@gmail.com>
2022-09-13 06:00:56 +0100

    pointer

--- learngo/main.go
+++ learngo/main.go
@@ -4,12 +4,16 @@ import (
 		"fmt"
 )
 
-func main() {
-		for i := 0; i < 5; i++ {
-				sayMessage("Hello Go", i)
-		}
-}
-func sayMessage(msg string, idx int) {
-		fmt.Println(msg)
-		fmt.Println("The value of the index is", idx)
+ func main() {
+		greeting := "Hello"
+		name := "Stacey"
+		sayGreeting(&greeting, &name)
+		fmt.Println(name)
+
+ }
+
+func sayGreeting(greeting, name *string) {
+		fmt.Println(*greeting, *name)
+		*name = "Ted"
+		fmt.Println(*name)
 }
