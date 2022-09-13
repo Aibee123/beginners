c606343af8d2cb8a7bde8e339c611519d6fcb123
ibbob90@gmail.com <ibbob90@gmail.com>
2022-09-13 13:32:25 +0100

    anonymous function

--- learngo/main.go
+++ learngo/main.go
@@ -5,17 +5,8 @@ import (
 )
 
 func main() {
-		d, err := divide(5.0, 3.0)
-		if err != nil {
-				fmt.Println(err)
-				return
-		}
-		fmt.Println(d)
-}
-
-func divide(a, b float64) (float64, error) {
-		if b == 0.0 {
-				return 0.0, fmt.Errorf("Cannot divide by zero")
-		}
-		return a / b, nil
+		func() {
+				msg := "Hello Go!"
+				fmt.Println(msg)
+		}()
 }
