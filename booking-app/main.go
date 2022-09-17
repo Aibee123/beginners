3dba2a60c6be1a3689f1703f004c8b2607577ff3
ibbob90@gmail.com <ibbob90@gmail.com>
2022-09-17 17:48:25 +0100

    remaining tickets

--- booking-app/main.go
+++ booking-app/main.go
@@ -6,11 +6,12 @@ func main() {
 	var conferenceName = "Go Conference"
 	const conferenceTickets = 50
 	var remainingTickets = 50
-
+	var bookings [50]string
+	
 	fmt.Printf("Welcome to %v booking application\n", conferenceName)
 	fmt.Printf("We have a total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
 	fmt. Println("Get your tickets here to attend")
-  
+	
 	var firstName string
 	var lastName string
 	var email string
@@ -29,5 +30,14 @@ func main() {
 	fmt.Println("Enter number of tickets: ")
 	fmt.Scan(&userTickets)
 
+	remainingTickets = remainingTickets - userTickets
+	bookings[0] = firstName + "" + lastName
+
+	fmt.Printf("The whole array: %v\n", bookings)
+	fmt.Printf("The first value: %v\n", bookings[0])
+	fmt.Printf("Array type: %T\n", bookings)
+	fmt.Printf("Array length: %v\n", len(bookings))
+
 	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
+	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
 }
