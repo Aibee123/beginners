6f3f4df90902b92007b5dc8dc5cec13eb5b3f321
ibbob90@gmail.com <ibbob90@gmail.com>
2022-09-18 15:26:11 +0100

    if statement

--- booking-app/main.go
+++ booking-app/main.go
@@ -5,11 +5,12 @@ import (
 	"strings"
 )
 
-func main() {
+	const conferenceTickets int = 50
 	var conferenceName = "Go Conference"
-	const conferenceTickets = 50
 	var remainingTickets = 50
-	var bookings []string
+	var bookings = []string{}
+
+	func main() {	
 	
 	fmt.Printf("Welcome to %v booking application\n", conferenceName)
 	fmt.Printf("We have a total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
@@ -34,16 +35,30 @@ func main() {
 		fmt.Println("Enter number of tickets: ")
 		fmt.Scan(&userTickets)
 
-		remainingTickets = remainingTickets - userTickets
-		bookings = append(bookings, firstName + " " + lastName)
+		if userTickets <= remainingTickets {
+			remainingTickets = remainingTickets - userTickets
+			bookings = append(bookings, firstName + " " + lastName)
+			
 
-		fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
-		fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
+			fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
+			fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
 
-		firstNames := []string{}
-		for _, booking := range bookings {
-			var names = strings.Fields(booking)
-			firstNames = append(firstNames, names[0])
-		}
+			firstNames := []string{}
+			for _, booking := range bookings {
+				var names = strings.Fields(booking)
+				firstNames = append(firstNames, names[0])
+			}
+			fmt.Printf("The first names of bookings are: %v\n", firstNames)
+		
+			if remainingTickets == 0 {
+				// end program
+				fmt.Printf("Our conference is booked out, Come back next year")
+				break 
+			}
+		}else {
+			fmt.Printf("We only have %v tickets remaining, so you can't book %v tickets\n", remainingTickets, userTickets)
+			continue
+		}	
 	}
 }
+
